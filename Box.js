d5680ae0e26b6709b97f2d6b22a4e605eba20fc4
AbeerNarang <70014201+AbeerNarang@users.noreply.github.com>
2020-10-13 19:35:39 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box {
+    constructor(x, y, width, height) {
+    var options = {
+    isStatic:false,    
+    restitution:0,
+    friction:1,
+    }
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    World.add(world, this.body);
+    }
+    display(){
+    var pos =this.body.position;
+    push();
+    var angle=this.body.angle;
+    translate(pos.x,pos.y);
+    rectMode(CENTER);
+    fill("blue");
+    rect(0,0, this.width, this.height);
+    pop();
+    }
+    }; 
